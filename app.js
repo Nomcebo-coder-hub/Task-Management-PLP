1424838cefbdd0ac16a2402be13a5eecfef03d3a
Nomcebo-coder-hub <nmthembu2797@gmail.com>
2024-06-12 09:02:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ firebase.initializeApp({
   apiKey: "AIzaSyD7xkhaAKCNq2gz_0_aO-plPHa3Yv7SXgQ",
   authDomain: "plp-firebase-project.firebaseapp.com",
   projectId: "plp-firebase-project",
+  storageBucket: "plp-firebase-project.appspot.com",
+  messagingSenderId: "706358347205",
+  appId: "1:706358347205:web:ef815464ce707e96a277ac",
 });
 
 const db = firebase.firestore();
